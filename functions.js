93e3e4257e6b74a5fb4503687cd545738da4966d
MdMahtabUddin <mahtab.sentu@outlook.com>
2023-09-17 03:01:03 +0600

    test some code by using Jest

--- functions.js
+++ functions.js
@@ -4,6 +4,14 @@ const functions = {
     add: (num1, num2) => num1 + num2,
     
     isNull: () => null,
+    checkValue:(x)=>x,
+    createUser : ()=>{
+        const user ={firstName:'Md Mahtab'}
+        user['lastName'] ='Uddin';
+
+        return user;
+
+    }
 
    
 }
--- functions.test.js
+++ functions.test.js
@@ -1,16 +1,48 @@
 const functions =require('./functions');
 
+
+// toBe 
 test('2+2 equal to 4',()=>{
     expect(functions.add(2,2)).toBe(4);
 })
 
+
+// not
 test('2+2 Not equal to 5',()=>{
     expect(functions.add(2,2)).not.toBe(5);
 })
 
 
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+// toBeNull 
 test('should be null',()=>{
     
     expect(functions.isNull()).toBeNull();
 })
+
+// toBeFalsy
+test('Should be falsy', () => {
+    expect(functions.checkValue(undefined)).toBeFalsy();
+  });
+
+// toEqual 
+test ('object should be Md Mahtab Uddin',()=>{
+    expect(functions.createUser()).toEqual({firstName:'Md Mahtab',lastName:'Uddin'
+    })
+})
+
+
+// Less then to Greater then 
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 800;
+    expect(load1 + load2).toBeLessThanOrEqual(1600);
+  });
+
 
